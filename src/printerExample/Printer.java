cd229e2ea4ed30f62ec0149027765fdaea863a34
wjseoduq3 <dragon65@nate.com>
2024-05-14 17:10:47 +0900

    1'st commit

---
+++ src/printerExample/Printer.java
@@ -0,0 +1,20 @@
+package printerExample;
+
+public class Printer {
+	
+	public void println(int a) {
+		System.out.println(a);
+	}
+		
+	public void println(boolean a) {
+		System.out.println(a);
+	}
+	
+	public void println(double a) {
+		System.out.println(a);
+	}
+	
+	public void println(String a) {
+		System.out.println(a);
+	}
+}
---
+++ src/printerExample/PrinterExample.java
@@ -0,0 +1,15 @@
+package printerExample;
+
+public class PrinterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Printer printer = new Printer();
+		
+		printer.println(10);
+		printer.println(false);
+		printer.println(5.7);
+		printer.println("홍길동");	
+	}
+}
